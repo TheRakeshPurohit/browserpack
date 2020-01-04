6115a031490f22ebb3f7bff37908a56ec5637180
Ameer Jhan <ameerjhanprof@gmail.com>
2020-01-04 18:13:21 +0530

    fix: scripts

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,9 +1,8 @@
-if [ -z "$1" ]; then 
-  echo "Provide type of update major, minor, patch"
+if [ -z $(git diff-index --quiet HEAD) ]; then 
+  echo "Can't publish git is not clean"
   exit 1;
 else 
    cp package.json dist/
    cp README.md dist/
    cp LICENSE dist/
-   npm version $1
 fi
